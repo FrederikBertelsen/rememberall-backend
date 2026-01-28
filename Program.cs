612806fef2ddf0d4091d48a8bb5b508f607af3e6
Frederik Bertelsen <frbb@itu.dk>
2026-01-29 00:15:24 +0100

    add: repositories to program.cs, swagger API Docs, ensure DB created on upstart.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using rememberall.src.Data;
+using rememberall.src.Repositories;
+using rememberall.src.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITodoListRepository, TodoListRepository>();
+builder.Services.AddScoped<ITodoItemRepository, TodoItemRepository>();
+builder.Services.AddScoped<IListCollaboratorRepository, ListCollaboratorRepository>();
+builder.Services.AddScoped<IInviteRepository, InviteRepository>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=app.db"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    // Host the Swagger UI at the application's root ("/")
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
@@ -19,5 +39,20 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Ensure database exist and apply any pending migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    try
+    {
+        db.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogError(ex, "An error occurred while initialising the database.");
+        throw;
+    }
+}
 
 app.Run();
