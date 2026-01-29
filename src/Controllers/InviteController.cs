2e1e60582f191d8ef34c21f5765b03bf8fd40cfb
Frederik Bertelsen <frbb@itu.dk>
2026-01-29 19:35:22 +0100

    add: Invite controller

---
+++ src/Controllers/InviteController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using RememberAll.src.DTOs;
+using RememberAll.src.Services.Interfaces;
+
+namespace RememberAll.src.Controllers;
+
+[Route("api/invites")]
+[ApiController]
+public class InviteController(IInviteService inviteService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult<InviteDto>> CreateInvite(CreateInviteDto newInviteDto)
+    {
+        var createdInviteDto = await inviteService.CreateInviteAsync(newInviteDto);
+        return Ok(createdInviteDto);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ICollection<InviteDto>>> GetSentInvitesByUserId(Guid userId)
+    {
+        var invites = await inviteService.GetSentInvitesByUserIdAsync(userId);
+        return Ok(invites);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ICollection<InviteDto>>> GetRecievedInvitesByUserId(Guid userId)
+    {
+        var invites = await inviteService.GetRecievedInvitesByUserIdAsync(userId);
+        return Ok(invites);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteInvite(Guid inviteId)
+    {
+        await inviteService.DeleteInviteByIdAsync(inviteId);
+        return NoContent();
+    }
+}
