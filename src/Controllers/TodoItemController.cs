31b460098956a1b7265de8fd1fc9330ba6aae438
Frederik Bertelsen <frbb@itu.dk>
2026-01-29 19:29:52 +0100

    add: TodoItem Controller

---
+++ src/Controllers/TodoItemController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using RememberAll.src.DTOs;
+using RememberAll.src.Services.Interfaces;
+
+namespace RememberAll.src.Controllers;
+
+public class TodoItemController(ITodoItemService todoItemService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult<TodoItemDto>> CreateTodoItem(CreateTodoItemDto newTodoItemDto)
+    {
+        var createdTodoItemDto = await todoItemService.CreateTodoItemAsync(newTodoItemDto);
+        return Ok(createdTodoItemDto);
+    }
+
+    [HttpPatch]
+    public async Task<ActionResult<TodoItemDto>> UpdateTodoItem(TodoItemDto todoItemDto)
+    {
+        var updatedTodoItemDto = await todoItemService.UpdateTodoItem(todoItemDto);
+        return Ok(updatedTodoItemDto);
+    }
+
+    [HttpPatch]
+    public async Task<ActionResult<TodoItemDto>> MarkTodoItemAsComplete(Guid itemId)
+    {
+        var updatedTodoItemDto = await todoItemService.MarkTodoItemAsCompleteAsync(itemId);
+        return Ok(updatedTodoItemDto);
+    }
+
+    [HttpPatch]
+    public async Task<ActionResult<TodoItemDto>> MarkTodoItemAsIncomplete(Guid itemId)
+    {
+        var updatedTodoItemDto = await todoItemService.MarkTodoItemAsIncompleteAsync(itemId);
+        return Ok(updatedTodoItemDto);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteTodoItem(Guid itemId)
+    {
+        await todoItemService.DeleteTodoItem(itemId);
+        return NoContent();
+    }
+}
