7f5002564ef6b2231e4e302685b750736fed6093
Frederik Bertelsen <frbb@itu.dk>
2026-01-29 11:41:10 +0100

    add: AsNoTracking() to get methods in todo list

--- src/Repositories/TodoListRepository.cs
+++ src/Repositories/TodoListRepository.cs
@@ -7,14 +7,19 @@ namespace rememberall.src.Repositories;
 
 public class TodoListRepository(AppDbContext dbContext) : ITodoListRepository
 {
-    public async Task<TodoList> CreateTodoListAsync(TodoList todoList) => 
+    public async Task<TodoList> CreateTodoListAsync(TodoList todoList) =>
         (await dbContext.TodoLists.AddAsync(todoList)).Entity;
-    public async Task<TodoList?> GetTodoListByIdAsync(Guid listId) => 
-        await dbContext.TodoLists.Include(todoList => todoList.Items).FirstOrDefaultAsync(todoList => todoList.Id == listId);
-    public async Task<ICollection<TodoList>> GetTodoListsByUserIdAsync(Guid userId) => 
-        await dbContext.TodoLists.Where(todoList => todoList.OwnerId == userId).ToListAsync();
-    public void AddItemToTodoList(TodoList todoList, TodoItem todoItem) => todoList.Items.Add(todoItem);
-    public void RemoveItemFromTodoList(TodoList todoList, TodoItem todoItem) => todoList.Items.Remove(todoItem);
+    public async Task<TodoList?> GetTodoListByIdAsync(Guid listId) =>
+        await dbContext.TodoLists
+                .AsNoTracking()
+                .Include(todoList => todoList.Items)
+                .FirstOrDefaultAsync(todoList => todoList.Id == listId);
+    public async Task<ICollection<TodoList>> GetTodoListsByUserIdAsync(Guid userId) =>
+        await dbContext.TodoLists
+                .AsNoTracking()
+                .Where(todoList => todoList.OwnerId == userId)
+                .ToListAsync();
+
     public TodoList UpdateTodoList(TodoList todoList) => dbContext.TodoLists.Update(todoList).Entity;
 
     public void DeleteTodoList(TodoList todoList) => dbContext.TodoLists.Remove(todoList);
